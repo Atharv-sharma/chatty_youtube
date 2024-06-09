071f27be55fac546fc9d4e1014a500a645a69b37
Atharv-Sharma <sharmaatharv2908@gmail.com>
2024-06-09 16:55:27 +0530

    Initial commit

---
+++ lib/pages/frame/welcome/controller.dart
@@ -0,0 +1,15 @@
+import 'state.dart';
+import 'package:get/get.dart';
+
+class WelcomeController extends GetxController{
+  WelcomeController();
+
+  final title = "Chatty .";
+  final state = WelcomeState();
+
+  @override
+  void onReady(){
+    super.onReady();
+    print(" WelcomeController ");
+  }
+}
